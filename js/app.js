84ad6331defcaecd5f4c9794753184417d92e2fc
Hanna-Kaisa Alanne <hanna-kaisa.alanne@metropolia.fi>
2016-11-11 14:43:09 +0200

    files

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var myArray = [ 
+     { Name: "Projekti", Credits: 20, time: 200 }, 
+     { Name: "Algoritmit", Credits: 2, time: 40 },
+     { Name: "Videot", Credits: 5, time: 20 },
+     { Name: "Keikka", Credits: 1, time: 10 },
+     { Name: "Ohjelmointi", Credits: 9, time: 90 },
+     { Name: "Lopputyö", Credits: 17, time: 100 }
+    
+]
+console.log(myArray);
+var newValue = {
+    Name: "MOI",
+    Credits: 4,
+    time: 4
+}
+myArray.push(newValue);
